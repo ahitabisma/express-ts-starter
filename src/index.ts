5081cabf1438680662bdaa0756d4e4bbe3a10ee2
Ahita Bisma Adlula <ahitabisma21@gmail.com>
2025-05-23 05:36:23 +0700

    Add cors configuration, morgan for http logging & fix get current user middleware

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,32 @@ import { authRoutes } from './routes/auth.routes';
 import { errorMiddleware } from './middlewares/error.middleware';
 import logger from './config/logger';
 import { userRoutes } from './routes/user.routes';
+import cors from 'cors';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 const app = express();
 
+const morganStream = {
+    write: (message: string) => {
+        logger.info(message.trim());
+    },
+};
+
+app.use(morgan('dev', { stream: morganStream }));
+
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL : '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+app.use
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Folder for static files photo profile users
